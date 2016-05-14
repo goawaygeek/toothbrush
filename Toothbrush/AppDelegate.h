13ecbe9b19c9c2e503241fa6c9ce32b9dc45406c
goawaygeek <scott@artprocessors.net>
2016-05-14 15:16:56 -0700

    added toothbrush

---
+++ Toothbrush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Toothbrush
+//
+//  Created by Scott Brewer on 5/14/16.
+//  Copyright © 2016 Organization of Stupid Scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Toothbrush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Toothbrush
+//
+//  Created by Scott Brewer on 5/14/16.
+//  Copyright © 2016 Organization of Stupid Scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
