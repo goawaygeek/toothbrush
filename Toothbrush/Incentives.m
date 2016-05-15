59c3dc90f42e24f1405f7f41ec136aeb6a56551d
georgelu <georgelu@stanford.edu>
2016-05-15 15:12:58 -0700

    Update Incentives.m
    more weird and wonderful phrases

--- Toothbrush/Incentives.m
+++ Toothbrush/Incentives.m
@@ -34,7 +34,7 @@
 }
 
 - (void) goCoach:(NSTimer *)timer {
-    NSArray *stupidSpeech = @[@"Just ten more minutes!", @"Now brush your crowns", @"Think about your uncle's gross teeth", @"Trump 20 16", @"Visualize how beautiful your teeth will be",@"Your code just beautiful like your teeth", @"meeting a investor today", @"Presenting a pitch today", @"Using the best teethbrush technolodgy right now",@"Smile everyday make you and others happier"];
+    NSArray *stupidSpeech = @[@"Just ten more minutes!", @"Now brush your crowns", @"Think about your uncle's gross teeth", @"Trump 20 16", @"Visualize how beautiful your teeth will be",@"Why did the toothbrush cross the road? To get to other side. L O L", @"Give me 5 more strokes!", @"Put your shoulder into it.",@"Hey, stop getting so much water on me",@"For best results, brush 3 times a day",@"Did you floss?",@"Your tongue could use more brushing",@"What do you think happens when you die?",@"Those cavities don't stand a chance",@"Tartar? I hardly even know her",@"Take that, plaque!",@"Nice teeth",@"Fun fact, the average American spends 3 years brushing teeth over his or her lifetime.",@"Brush at a 90 degree angle",@"Replace your toothbrush every month",@"Replace your toothbrush when you hear my screams",@"AAAAHHHHHHHH",@"Brush at least twice a day",@"You have really nice teeth",@"Heh",@"Give your gums a bit more attention",@"You're moving too fast",@"You're moving too slow"];
     
     NSUInteger randomIndex = arc4random() % [stupidSpeech count];
     
